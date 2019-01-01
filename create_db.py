eab2e4cd7012aea623a3f71f8b24afaac02b7c9b
martin-martin <breuss.martin@gmail.com>
2018-12-31 18:10:01 -0700

    improve db structure

--- create_db.py
+++ create_db.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlalchemy as sa
 import sqlalchemy.ext.declarative as dec
 
@@ -9,18 +10,20 @@ class File(Base):
 
     __tablename__ = "files"
 
+    # use BigInteger for Postgres, but doesn't work for SQLite
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    type = sa.Column(sa.String)
-    name = sa.Column(sa.String)
-    path = sa.Column(sa.String)
-    revisits = sa.Column(sa.Integer, default=0)
+    type = sa.Column(sa.String, nullable=True, index=True)
+    name = sa.Column(sa.String, nullable=True)
+    path = sa.Column(sa.String, nullable=False, unique=True)
+    # default is my birthday :)
+    last_revisit = sa.Column(sa.DateTime, nullable=True, default=datetime.datetime(1987, 1, 15), index=True)
+    revisits = sa.Column(sa.Integer, default=0, index=True)
 
     def __repr__(self):
-        return "{0}: {1}".format(self.name, self.revisits)
+        return "{0}: {1}".format(self.revisits, self.name)
 
 
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
+# Create an engine that stores data in the local directory's .db file.
 engine = sa.create_engine('sqlite:///revisit.db')
 
 # Create all tables in the engine. This is equivalent to "Create Table"
